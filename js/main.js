bb04bcbed6939b848a0641d53dd4a918ba97868b
Jason Serafica <seraficaj@gmail.com>
2022-02-03 14:54:00 -0800

    move hero around with keys

--- js/main.js
+++ js/main.js
@@ -1,55 +1,75 @@
 /* DOM SELECTORS */
 const canvas = document.querySelector("#canvas");
-/* GAME STATE/CANVAS RENDERING*/
-// set up the rendering
+document.addEventListener("keydown", movementHandler);
+// document.addEventListener('keydown', movementHandler) // stretch goals might be useful
+
+/* GAME STATE/CANVAS RENDERING STUFF */
+// setup the renderer
 const ctx = canvas.getContext("2d");
-//set canvas size to be the same as window
+// set canvas size to be the same as window
+// after the window computes -- set canvas to be actual size of space it takes up
 canvas.setAttribute("height", getComputedStyle(canvas)["height"]);
 canvas.setAttribute("width", getComputedStyle(canvas)["width"]);
-// set up the game loop
-const gameLoopInterval = setInterval(gameLoop, 60) // run every 60 milliseconds
+// setup the gameloop
+const gameLoopInterval = setInterval(gameLoop, 60); // game logic + rendering tied to speed loop runs
 
-// console.log(ctx);
+// console.log(ctx)
 
 /* GAME FUNCTIONS */
-// console.log(canvas);
+// console.log(canvas)
 
-// // set the color property of the context
-// ctx.fillStyle = "red";
+// set the color property of the context
+// ctx.fillStyle = 'red' // any valid css color will
+// // set line width
 
-// // use a context rendering method
+// // use a context rending method
 // // (x position, y position, width, height)
-// ctx.fillRect(10, 10, 100, 100);
+// ctx.fillRect(10, 10, 100, 100)
 
-// //set line width
-// ctx.lineWidth = 30; // always in pixels
-// ctx.strokeStyle = "green";
+// ctx.lineWidth = 30 // always in pixels
+// ctx.strokeStyle = 'green'
 // // (x position, y position, width, height)
-// ctx.strokeRect(300, 300, 100, 100);
+// ctx.strokeRect(300, 300, 100, 100)
 
-// // encapsulate square drawing into function
-// function drawBox(x, y, width, height, color) {
-//     ctx.fillStyle = color;
-//     ctx.fillRect(x, y, width, height);
-// }
+// // encapsulate square drawing into a function
+function drawBox(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
 
-// drawBox(150, 150, 30, 30, 'purple');
-// drawBox(200, 150, 45, 60, '#FFFFFF');
+// drawBox(150, 150, 30, 30, 'purple')
+// drawBox(200, 150, 45, 60, '#FFFFFF') //white hex
 
 // data and functions to update the game element
+// encapslate methods and data that the mothods use together -- OOP ENCAPSULATION
 const hero = {
     x: 10,
     y: 10,
     width: 20,
     height: 20,
-    color: 'hotpink',
+    color: "hotpink",
     render: () => {
-        ctx.fillStyle = hero.color
-        ctx.fillRect(hero.x, hero.y, hero.width, hero.height)
-    }
+        ctx.fillStyle = hero.color;
+        ctx.fillRect(hero.x, hero.y, hero.width, hero.height);
+    },
 };
+
 const ogre = {};
 
-function gameLoop() {
-
+function movementHandler(e) {
+    console.log(e);
+    // conditional logic based on what key was pressed
+    if (e.key === "a" || e.key === "ArrowLeft") hero.x -= speed;
+    if (e.key === "d" || e.key === "ArrowRight") hero.x += speed;
+    if (e.key === "s" || e.key === "ArrowUp") hero.y += speed;
+    if (e.key === "w" || e.key === "ArrowDown") hero.y -= speed;
 }
+
+function gameLoop(){
+    // clear canvas and then render
+    // clear the canvas from left to bottom right to up
+    // render all gameplay elements (hero and ogre)
+    hero.render();
+    // gameplay logic -- win conditions ???
+    // collision detections
+};
