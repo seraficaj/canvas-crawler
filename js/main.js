d10929711030ea16bce5937174393c746d580e76
Jason Serafica <seraficaj@gmail.com>
2022-02-03 14:05:35 -0800

    make second square on canvas

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 /* DOM SELECTORS */
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector("#canvas");
 /* GAME STATE/CANVAS RENDERING*/
 // set up the rendering
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 //set canvas size to be the same as window
-canvas.setAttribute('height', getComputedStyle(canvas)["height"]);
-canvas.setAttribute('width', getComputedStyle(canvas)["width"]);
+canvas.setAttribute("height", getComputedStyle(canvas)["height"]);
+canvas.setAttribute("width", getComputedStyle(canvas)["width"]);
 // console.log(ctx);
 
 /* GAME FUNCTIONS */
 // console.log(canvas);
 
 // set the color property of the context
-ctx.fillStyle = 'red';
-//set line width
-// ctx.lineWidth = 10 // always in pixels
+ctx.fillStyle = "red";
 
 // use a context rendering method
 // (x position, y position, width, height)
-ctx.fillRect(10, 10, 100, 100)
+ctx.fillRect(10, 10, 100, 100);
+
+//set line width
+ctx.lineWidth = 30; // always in pixels
+ctx.strokeStyle = "green";
+// (x position, y position, width, height)
+ctx.strokeRect(300, 300, 100, 100);
